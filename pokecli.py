117b2be6503b6e731dba89a3df56e3d054646ddc
Reaver01 <reaver_01@gmx.com>
2016-07-21 18:48:53 +0200

    ssl fix

--- pokecli.py
+++ pokecli.py
@@ -34,6 +34,9 @@ import argparse
 import working
 import time
 import datetime
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 from pgoapi import PGoApi
 from pgoapi.utilities import f2i, h2f
